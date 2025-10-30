09ec455af12c52f858a3eb727a51f41225d97406
Abimanyu-Nayagam <abimanyunayagam21@gmail.com>
2025-10-31 01:14:16 +0530

    Add Pydantic model data validation

--- app/routes/export_route.py
+++ app/routes/export_route.py
@@ -1,7 +1,6 @@
 from io import BytesIO
 import json
 from app.models.db_models import Entry, Snippet, db
-from app.models.models import ExportEntryRequest, ExportSnippetRequest
 from flask import Blueprint, request, jsonify, send_file
 
 e_bp = Blueprint('export_route', __name__)
@@ -14,7 +13,9 @@ def export_entry_md(entry_id):
     Parameters:
         entry_id: id of the entry which needs to be exported
     '''
-
+    if type(entry_id) != int:
+        return jsonify({'error': 'Invalid id format. id must be an integer.'}), 400
+    
     id = entry_id
 
     if not id:
@@ -51,6 +52,9 @@ def export_snippet_md(snippet_id):
         snippet_id: id of the snippet which needs to be exported
     '''
 
+    if type(snippet_id) != int:
+        return jsonify({'error': 'Invalid id format. id must be an integer.'}), 400
+
     id = snippet_id
 
     if not id:
@@ -78,16 +82,18 @@ def export_snippet_md(snippet_id):
         mimetype="text/markdown"
         ), 200
 
-@e_bp.route('/export-snippet-json/v1/<int:entry_id>',methods=['GET'])
-def export_snippet_json(entry_id):
+@e_bp.route('/export-snippet-json/v1/<int:snippet_id>',methods=['GET'])
+def export_snippet_json(snippet_id):
     '''
     Export the selected snippet in the form of a json (.json) file using the provided entry id
 
     Parameters:
         snippet_id: id of the snippet which needs to be exported
     '''
+    if type(snippet_id) != int:
+        return jsonify({'error': 'Invalid id format. id must be an integer.'}), 400
 
-    id = entry_id
+    id = snippet_id
 
     if not id:
         return jsonify({'error': 'id is required for exporting.'}), 400  #Validate Each value
@@ -125,6 +131,9 @@ def export_entry_json(entry_id):
     Parameters:
         snippet_id: id of the entry which needs to be exported
     '''
+
+    if type(entry_id) != int:
+        return jsonify({'error': 'Invalid id format. id must be an integer.'}), 400
 
     id = entry_id
 
