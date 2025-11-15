28dbc0203b4dd9b05b00d230e47c9e4324b8fb1f
YSH-NYK <yashnaik7664@gmail.com>
2025-11-15 11:01:57 +0530

    Add packagesand modify app_run.py to run both flask and react

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,10 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
-import './styles.css'
+import './style.css'
 
 createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
 )
