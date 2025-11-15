bfe4bd0ceb03fb447dcf2a5245f1a355cc501019
YSH-NYK <yashnaik7664@gmail.com>
2025-11-15 10:35:39 +0530

    Add Frontend directory structure

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from .models.db_models import db
 from .routes.route import bp as routes_bp
 from .routes.export_route import e_bp as export_routes_bp
@@ -8,12 +9,10 @@ from config.config import Config
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+    CORS(app) # Enable CORS for all routes to resolve cross-origin issues (React and Flask running on different ports)
     db.init_app(app)
     jwt.init_app(app)
     app.register_blueprint(routes_bp) #Register the route blueprint
     app.register_blueprint(export_routes_bp) #Register the file export route blueprint
     app.register_blueprint(auth_bp) # Register the auth blueprint
     return app
-
-
-
