3fa26729861a58c9596c8be950f0a39951fb0167
YSH-NYK <yashnaik7664@gmail.com>
2025-11-22 15:01:30 +0530

    Add proper blueprint imports in __init__.py

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from flask_cors import CORS
 from .models.db_models import db
-from .routes.route import bp as routes_bp
+from .routes.crud_route import bp as crud_routes_bp
 from .routes.export_route import e_bp as export_routes_bp
 from .routes.autogen_route import autogen_bp
-from .routes.auth import auth_bp, jwt
+from .routes.auth_route import auth_bp, jwt
 from config.config import Config
 
 def create_app(config_class=Config):
@@ -13,7 +13,7 @@ def create_app(config_class=Config):
     CORS(app) # Enable CORS for all routes to resolve cross-origin issues (React and Flask running on different ports)
     db.init_app(app)
     jwt.init_app(app)
-    app.register_blueprint(routes_bp) #Register the route blueprint
+    app.register_blueprint(crud_routes_bp) #Register the route blueprint
     app.register_blueprint(export_routes_bp) #Register the file export route blueprint
     app.register_blueprint(auth_bp) # Register the auth blueprint
     app.register_blueprint(autogen_bp) # Register the autogen blueprint
